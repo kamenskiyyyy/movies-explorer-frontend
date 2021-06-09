a098fac8d4953a18fa92db87b3f1014f428c9c54
Kamenev Nikolay <79969997909@mail.ru>
2021-06-09 16:25:01 +0300

    create project's infrastructure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,39 @@
-import logo from '../../logo.svg';
 import './App.css';
+import {Switch, Route} from "react-router-dom";
+import About from "../Main/Main";
+import Movies from "../Movies/Movies";
+import SavedMovies from "../SavedMovies/SavedMovies";
+import Profile from "../Profile/Profile";
+import Register from "../Register/Register";
+import Login from "../Login/Login";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page__container'>
+      <Header/>
+      <Switch>
+        <Route path='/signup'>
+          <Register/>
+        </Route>
+        <Route path='/signin'>
+          <Login/>
+        </Route>
+        <Route path='/profile'>
+          <Profile/>
+        </Route>
+        <Route path='/saved-movies'>
+          <SavedMovies/>
+        </Route>
+        <Route path='/movies'>
+          <Movies/>
+        </Route>
+        <Route exact path="/">
+          <About/>
+        </Route>
+      </Switch>
+      <Footer/>
     </div>
   );
 }

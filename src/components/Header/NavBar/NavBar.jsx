7bd60632b9cd0ea52f83ac51a44e6d0abfcdae43
Kamenev Nikolay <79969997909@mail.ru>
2021-06-14 12:16:33 +0300

    upd Header, add authorization state

--- src/components/Header/NavBar/NavBar.jsx
+++ src/components/Header/NavBar/NavBar.jsx
@@ -1,22 +1,33 @@
 import './NavBar.css';
 import {NavLink} from "react-router-dom";
+import icon from '../../../images/profile-icon.svg';
 
 function NavBar() {
   return (
     <>
-      {/* Меню неавторизованный пользователь */}
+      {/*/!* Меню неавторизованный пользователь *!/ // В настоящий момент скрыто, из-за отсутствия провайдера*/}
+      {/*<nav className='nav'>*/}
+      {/*  <ul className='nav__list'>*/}
+      {/*    <li>*/}
+      {/*      <NavLink className='nav__link' to='/signup'>Регистрация</NavLink>*/}
+      {/*    </li>*/}
+      {/*    <li>*/}
+      {/*      <NavLink className='nav__link nav__link_green' to='/signin'>Войти</NavLink>*/}
+      {/*    </li>*/}
+      {/*  </ul>*/}
+      {/*</nav>*/}
+
+      {/* Меню авторизованный пользователь */}
       <nav className='nav'>
         <ul className='nav__list'>
-          <li>
-            <NavLink className='nav__link' to='/signup'>Регистрация</NavLink>
-          </li>
+          <li><NavLink activeClassName='nav__link_active' className='nav__link' to='/movies'>Фильмы</NavLink></li>
+          <li><NavLink activeClassName='nav__link_active' className='nav__link' to='/saved-movies'>Сохраненные фильмы</NavLink></li>
         </ul>
         <ul className='nav__list'>
           <li>
-            <NavLink className='nav__link nav__link_green' to='/signin'>Войти</NavLink>
+            <NavLink className='nav__link' to='/signup'>Аккаунт <img className='nav__link_icon' src={icon} alt="Иконка профиля"/></NavLink>
           </li>
         </ul>
-
       </nav>
     </>
   )

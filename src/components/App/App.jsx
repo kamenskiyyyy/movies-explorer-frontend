57694d445a308190e19ec3a421b0f888d05df71a
Kamenev Nikolay <79969997909@mail.ru>
2021-06-20 21:49:09 +0300

    add Context and ProtectedRoute

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import {Switch, Route} from "react-router-dom";
+import {Switch, Route, useHistory} from "react-router-dom";
 import About from "../Main/Main";
 import Movies from "../Movies/Movies";
 import SavedMovies from "../SavedMovies/SavedMovies";
@@ -10,15 +10,27 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import NotFound from "../NotFound/NotFound";
 import InfoTooltip from "../InfoTooltip/InfoTooltip";
-import {useCallback, useState} from "react";
-import {statusSuccessMessage} from "../../utils/constants";
+import {useCallback, useEffect, useState} from "react";
+import {statusErrors, statusErrorText, statusSuccessMessage} from "../../utils/constants";
 import statusSuccessImage from '../../images/success.svg';
+import statusErrorImage from '../../images/error.svg';
+import {AppContext} from "../../contexts/AppContext";
+import {CurrentUserContext} from "../../contexts/CurrentUserContext";
+import {auth} from "../../utils/MainApi";
+import ProtectedRoute from "../../hoc/ProtectedRoute";
+import {api} from "../../utils/MoviesApi";
 
 function App() {
+  const history = useHistory();
+  const [loggedIn, setLoggedIn] = useState(false);                                                   // Стейт-переменная статус пользователя, вход в систему
   const [infoTooltip, setInfoTooltip] = useState({                                                   // Стейт информационного попапа статуса
     isOpen: false,
     image: statusSuccessImage,
     message: statusSuccessMessage
+  });
+  const [currentUser, setCurrentUser] = useState({                                                   // Стейт данные текущего пользователя
+    name: '',
+    email: ''
   });
 
   // Функция закрытия всех попапов
@@ -29,45 +41,136 @@ function App() {
     });
   }, [infoTooltip])
 
+  // Обработчик по кнопке Войти
+  function handleLogin(evt, password, email) {
+    auth.authorize(password, email)
+      .then(() => {
+        setLoggedIn(true);
+        history.push('/movies');
+      })
+      .catch(err => handleError(evt.target, err));                                          // По указанным Логину и Паролю пользователь не найден. Проверьте введенные данные и повторите попытку.
+  }
+
+  // Обработчик ошибки по кнопке Войти
+  function handleError(form, statusError) {
+    const errors = statusErrors.filter(error => error.name === form.name)[0].errors;
+    const statusErrorMessage = errors.filter(error => error.status === statusError)[0].message;
+    setInfoTooltip({
+      ...infoTooltip,
+      isOpen: true,
+      image: statusErrorImage,
+      message: statusErrorMessage ? statusErrorMessage : statusErrorText
+    });
+  }
+
+  // Обработчик по кнопке Зарегистрироваться
+  function handleRegister(evt, name, password, email) {
+    auth.register(name, password, email)
+      .then(() => {
+        setInfoTooltip({
+          ...infoTooltip,
+          isOpen: true,
+          image: statusSuccessImage,
+          message: statusSuccessMessage
+        });
+        setLoggedIn(true);
+        history.push('/movies');
+      })
+      .catch(err => handleError(evt.target, err));                                                                // Обработка ошибки handleError();
+  }
+
+  // // Обработчик обновления информации пользователя
+  function handleUpdateUser(userInfo) {
+    api.editProfile(userInfo)
+      .then(data => {
+        setCurrentUser({...data});
+      })
+      .catch(err => console.log(err));
+  }
+
+  // // Проверка токена при повторном посещении сайта
+  const tokenCheck = useCallback(() => {
+    const token = localStorage.getItem('jwt');
+    if (token) {
+      auth.getContent(token)
+        .then(res => {
+          if (res) {
+            setLoggedIn(true);
+            history.push('/movies')
+          }
+        })
+        .catch(err => {
+          console.log(err)
+          localStorage.removeItem('jwt')
+        })
+    } else {
+      setLoggedIn(false);
+    }
+  }, [history])
+
+  useEffect(() => {
+    tokenCheck();
+  }, [tokenCheck]);
+
+  // Выход из аккаунта
+  function signOut() {
+    setLoggedIn(false);
+    localStorage.removeItem('jwt');
+    history.push('/');
+  }
+
+  // Загрузка данных пользователя
+  useEffect(() => {
+    api.getUserInfo()
+      .then(data => {
+        setCurrentUser({...data});
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
-    <div className='page__container'>
-      <Switch>
-        <Route path='/signup'>
-          <Register/>
-        </Route>
-        <Route path='/signin'>
-          <Login/>
-        </Route>
-        <Route path='/profile'>
-          <Header/>
-          <Profile/>
-        </Route>
-        <Route path='/saved-movies'>
-          <Header/>
-          <SavedMovies/>
-          <Footer/>
-        </Route>
-        <Route path='/movies'>
-          <Header/>
-          <Movies/>
-          <Footer/>
-        </Route>
-        <Route exact path="/">
-          <Header/>
-          <About/>
-          <Footer/>
-        </Route>
-        <Route path='*'>
-          <NotFound/>
-        </Route>
-      </Switch>
-      <InfoTooltip
-        isOpen={infoTooltip.isOpen}
-        onClose={closePopup}
-        statusImage={infoTooltip.image}
-        statusMessage={infoTooltip.message}
-      />
-    </div>
+    <AppContext.Provider value={{loggedIn, handleLogin, signOut}}>
+      <CurrentUserContext.Provider value={currentUser}>
+        <div className='page__container'>
+          <Switch>
+            <Route path='/signup'>
+              <Register handleRegister={handleRegister}
+                        handleError={handleError}/>
+            </Route>
+            <Route path='/signin'>
+              <Login handleLogin={handleLogin}
+                     handleError={handleError}/>
+            </Route>
+            <ProtectedRoute path='/profile'>
+              <Header isLogin={loggedIn}/>
+              <Profile onUpdateUser={handleUpdateUser} />
+            </ProtectedRoute>
+            <ProtectedRoute path='/saved-movies'>
+              <Header isLogin={loggedIn}/>
+              <SavedMovies/>
+              <Footer/>
+            </ProtectedRoute>
+            <ProtectedRoute path='/movies'>
+              <Header isLogin={loggedIn}/>
+              <Movies/>
+              <Footer/>
+            </ProtectedRoute>
+            <Route exact path="/">
+              <Header isLogin={loggedIn}/>
+              <About/>
+              <Footer/>
+            </Route>
+            <Route path='*'>
+              <NotFound/>
+            </Route>
+          </Switch>
+          <InfoTooltip isOpen={infoTooltip.isOpen}
+                       onClose={closePopup}
+                       statusImage={infoTooltip.image}
+                       statusMessage={infoTooltip.message}/>
+        </div>
+      </CurrentUserContext.Provider>
+    </AppContext.Provider>
   );
 }
 
---
+++ src/hoc/ProtectedRoute.js
@@ -0,0 +1,14 @@
+import {useContext} from 'react';
+import {Route, Redirect} from 'react-router-dom';
+import {AppContext} from '../contexts/AppContext';
+
+function ProtectedRoute(props) {
+  const value = useContext(AppContext);
+  return (
+    <Route>
+      {value.loggedIn ? props.children : <Redirect to="/signin"/>}
+    </Route>
+  )
+}
+
+export default ProtectedRoute;

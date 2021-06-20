1fb57d97e2c1dcf264de2b8f376aa8aa3bd0e26e
Kamenev Nikolay <79969997909@mail.ru>
2021-06-20 21:47:18 +0300

    upd functional login and register

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,26 +1,51 @@
 import {Link, NavLink} from "react-router-dom";
 import logo from "../../images/logo.svg";
+import {useState} from "react";
 
-function Login() {
+function Login({handleLogin}) {
+  const [userData, setUserData] = useState({
+    password: '',
+    email: ''
+  })
+
+  function handleChange(evt) {
+    const {name, value} = evt.target;
+    setUserData({
+      ...userData,
+      [name]: value
+    })
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    const {password, email} = userData;
+    if (!password || !email) {
+      return;
+    }
+    handleLogin(evt, password, email);
+  }
+
   return (
     <main className='register'>
       <NavLink className='header__logo register__logo' to='/'><img src={logo} alt="Логотип"/></NavLink>
       <h1 className='register__head'>Рады видеть!</h1>
-      <form className='register__form'>
+      <form className='register__form' onSubmit={handleSubmit}>
         <label className='register__form_label'>E-mail</label>
         <input
           required
           autoComplete='off'
           className='register__form_input'
           type="email"
-          name="email"/>
+          name="email"
+          onChange={handleChange}/>
         <label className='register__form_label'>Пароль</label>
         <input
           required
           autoComplete='off'
           className='register__form_input error'
           type="password"
-          name="password"/>
+          name="password"
+          onChange={handleChange}/>
         <button type='submit' className='register_btn'>Зарегистрироваться</button>
       </form>
       <div className='register__in'>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,34 +1,58 @@
 import './Register.css';
 import logo from '../../images/logo.svg';
 import {Link, NavLink} from "react-router-dom";
+import {useState} from "react";
 
-function Register() {
+function Register({handleRegister}) {
+  const [userData, setUserData] = useState({
+    name: '',
+    password: '',
+    email: ''
+  });
+
+  function handleChange(evt) {
+    const {name, value} = evt.target;
+    setUserData({
+      ...userData,
+      [name]: value
+    })
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    const {name, password, email} = userData;
+    handleRegister(evt, name, password, email);
+  }
+
   return (
     <main className='register'>
       <NavLink className='header__logo register__logo' to='/'><img src={logo} alt="Логотип"/></NavLink>
       <h1 className='register__head'>Добро пожаловать!</h1>
-      <form className='register__form'>
+      <form className='register__form' onSubmit={handleSubmit}>
         <label className='register__form_label'>Имя</label>
         <input
           required
           autoComplete='off'
           className='register__form_input'
           type="text"
-          name="name"/>
+          name="name"
+          onChange={handleChange}/>
         <label className='register__form_label'>E-mail</label>
         <input
           required
           autoComplete='off'
           className='register__form_input'
           type="email"
-          name="email"/>
+          name="email"
+          onChange={handleChange}/>
         <label className='register__form_label'>Пароль</label>
         <input
           required
           autoComplete='off'
           className='register__form_input error'
           type="password"
-          name="password"/>
+          name="password"
+          onChange={handleChange}/>
         <span className='register__form_span'>Что-то пошло не так...</span>
         <button type='submit' className='register_btn'>Зарегистрироваться</button>
       </form>
